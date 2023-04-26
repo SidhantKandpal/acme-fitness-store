66a8b96f227b841fd237717bfa90ca2cf6f81478
SidhantKandpal <69434793+SidhantKandpal@users.noreply.github.com>
2023-04-26 07:23:57 +0000

    Update setup-env-variables-template.sh

--- scripts/setup-env-variables-template.sh
+++ scripts/setup-env-variables-template.sh
@@ -1,6 +1,6 @@
-export SUBSCRIPTION=CHANGE-ME                 # replace it with your subscription-id
-export RESOURCE_GROUP=acme-fitness-rg           # existing resource group or one that will be created in next steps
-export SPRING_APPS_SERVICE=acme-fitness-YOUR-UNIQUE-USERNAME   # A unique name of the service that will be created in the next steps
+export SUBSCRIPTION=2b662324-593f-4cb9-9e69-7c7ca8916694                 # replace it with your subscription-id
+export RESOURCE_GROUP=asc-training-asae-ifdu-131           # existing resource group or one that will be created in next steps
+export SPRING_APPS_SERVICE=acme-fitness-Tester   # A unique name of the service that will be created in the next steps
 export LOG_ANALYTICS_WORKSPACE=acme-fitness-la-CHANGE-ME   # existing workspace or one that will be created in next steps
 export REGION=canadacentral                          # choose a region with Enterprise tier support
 
